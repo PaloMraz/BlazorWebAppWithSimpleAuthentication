899be5d95e739f475ce643f262de66bbea21b7e1
Pavol Mráz <pavol.mraz@fores.group>
2025-04-03 08:37:36 +0200

    Add instance tracking, garbage collection, and payload allocation
    In AppComponentBase.cs, added static fields for tracking instance creation and destruction, with logging in the constructor and destructor.
    
    In Home.razor, introduced a button to manually trigger garbage collection via the CollectGarbage method.
    
    In Insecure.razor, added a private byte array _payload initialized to 100 MB in the OnInitialized method for memory allocation purposes.

--- Components/AppComponentBase.cs
+++ Components/AppComponentBase.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+using System.Diagnostics;
 
 namespace BlazorWebAppWithSimpleAuthentication.Components;
 
 public abstract class AppComponentBase : ComponentBase
 {
+  private static long _s_instanceIdSeed;
+  private static long _s_counter;
+  private readonly long _instanceId;
+
   protected AppComponentBase()
   {
+    this._instanceId = Interlocked.Increment(ref _s_instanceIdSeed);
+    Interlocked.Increment(ref _s_counter);
+    Debug.WriteLine($"=============> {this.GetType().Name} #{this._instanceId} created, total {_s_counter}.");
+  }
+
+  ~AppComponentBase()
+  {
+    Interlocked.Decrement(ref _s_counter);
+    Debug.WriteLine($"=============> {this.GetType().Name}#{this._instanceId} destroyed, total {_s_counter}");
   }
 
   protected async override Task OnInitializedAsync()
